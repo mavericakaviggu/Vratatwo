a3b8daada3e228d71a83cbdb78c9d8b3bc5fa35f
vignesh <vigneshshet4577@gamil.com>
2024-03-24 12:03:44 +0530

    junit testing

--- src/test/java/com/thinkconstructive/vratatwo/repository/CloudVendorRepositoryTest.java
+++ src/test/java/com/thinkconstructive/vratatwo/repository/CloudVendorRepositoryTest.java
@@ -9,11 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.thinkconstructive.vratatwo.model.CloudVendor;
 
-@SpringBootTest
 @DataJpaTest
 public class CloudVendorRepositoryTest {
 	
